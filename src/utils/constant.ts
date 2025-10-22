b571e0a5be704fcf0d17ccb97c275de9751b9615
PramodhReddy07 <pramodhreddy07@gmail.com>
2025-10-22 13:14:15 -0400

    Comment out education data in constants file

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -4,23 +4,23 @@ export const experienceData: I_Experience[] = [
    
 ];
 
-export const educationData: I_Education[] = [
-    {
-        institute_link: "https://www.iit.edu/",
-        institute_logo: "/iit.png",
-        course_title: "Masters in Computer Science",
-        ending_date: "",
-        institute_name: "Illinois Institute of Technology",
-        cgpa: 8.5,
-        description: ""
-    },
-    {
-        institute_link: "https://vnrvjiet.ac.in/",
-        institute_logo: "/vnrvjiet.jpeg",
-        course_title: "Bachelors of Engineering in Electrical and Electronics Engineering",
-        ending_date: "",
-        institute_name: "VNR Vignana Jyothi Institute of Engineering and Technology",
-        cgpa: 8.5,
-        description: ""
-    }
-] 
+// export const educationData: I_Education[] = [
+//     {
+//         institute_link: "https://www.iit.edu/",
+//         institute_logo: "/iit.png",
+//         course_title: "Masters in Computer Science",
+//         ending_date: "",
+//         institute_name: "Illinois Institute of Technology",
+//         cgpa: 8.5,
+//         description: ""
+//     },
+//     {
+//         institute_link: "https://vnrvjiet.ac.in/",
+//         institute_logo: "/vnrvjiet.jpeg",
+//         course_title: "Bachelors of Engineering in Electrical and Electronics Engineering",
+//         ending_date: "",
+//         institute_name: "VNR Vignana Jyothi Institute of Engineering and Technology",
+//         cgpa: 8.5,
+//         description: ""
+//     }
+// ] 
